fa62df54f64fd3d91eb92806f3d9b68ca16f5de1
changjun.lee <pord7457@gmail.com>
2024-01-22 20:01:42 +0900

    update memory access profiling

---
+++ optimization/cache/01_read.cpp
@@ -0,0 +1,33 @@
+#include <cstddef>
+#include <cstdlib>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+
+#include "benchmark/benchmark.h"
+
+#define REPEAT2(x) x x
+#define REPEAT4(x) REPEAT2(x) REPEAT2(x)
+#define REPEAT8(x) REPEAT4(x) REPEAT4(x)
+#define REPEAT16(x) REPEAT8(x) REPEAT8(x)
+#define REPEAT32(x) REPEAT16(x) REPEAT16(x)
+#define REPEAT(x) REPEAT32(x)
+
+template <class Word>
+void BM_read_rand(benchmark::State& state) {
+    volatile Word* const p = new Word;
+    ::memset((void*)p, 0xab, sizeof(Word));
+
+    for (auto _ : state) {
+        REPEAT(benchmark::DoNotOptimize(*p);)
+    }
+    delete p;
+    state.SetBytesProcessed(32*sizeof(Word)*state.iterations());
+    state.SetItemsProcessed(32*state.iterations());
+}
+
+
+BENCHMARK_TEMPLATE1(BM_read_rand, unsigned int);
+BENCHMARK_TEMPLATE1(BM_read_rand, unsigned long);
+
+BENCHMARK_MAIN();
--- optimization/pipelining/01_superscalar.cpp
+++ optimization/pipelining/01_superscalar.cpp
@@ -140,6 +140,53 @@ void BM_instructions(benchmark::State& state) {
     //state.SetBytesProcessed(N*sizeof(unsigned long)*state.iterations());
 }
 
+void BM_instructions8(benchmark::State& state) {
+    srand(1);
+    const unsigned int N = state.range(0);
+    std::vector<unsigned long> v1(N), v2(N), v3(N), v4(N);
+    std::vector<unsigned long> v5(N), v6(N), v7(N), v8(N);
+    for (size_t i = 0; i < N; ++i) {
+        v1[i] = rand();
+        v2[i] = rand();
+        v3[i] = rand();
+        v4[i] = rand();
+        v5[i] = rand();
+        v6[i] = rand();
+        v7[i] = rand();
+        v8[i] = rand();
+    }
+    unsigned long* p1 = v1.data();
+    unsigned long* p2 = v2.data();
+    unsigned long* p3 = v3.data();
+    unsigned long* p4 = v4.data();
+    unsigned long* p5 = v5.data();
+    unsigned long* p6 = v6.data();
+    unsigned long* p7 = v7.data();
+    unsigned long* p8 = v8.data();
+    for (auto _ : state) {
+        unsigned long a1 = 0, a2 = 0;
+        unsigned long a3 = 0, a4 = 0;
+        unsigned long a5 = 0, a6 = 0;
+        for (size_t i = 0; i < N; ++i) {
+            a1 += p1[i] + p2[i];
+            a2 += p3[i] * p4[i];
+            a3 += p5[i] << 2;
+            a4 += p5[i] - p6[i];
+            a5 += (p8[i] << 1) * p2[i];
+            a6 += (p7[i] - 3) * p1[i];
+        }
+        benchmark::DoNotOptimize(a1);
+        benchmark::DoNotOptimize(a2);
+        benchmark::DoNotOptimize(a3);
+        benchmark::DoNotOptimize(a4);
+        benchmark::DoNotOptimize(a5);
+        benchmark::DoNotOptimize(a6);
+        benchmark::ClobberMemory();
+    }
+    state.SetItemsProcessed(N*state.iterations());
+    //state.SetBytesProcessed(N*sizeof(unsigned long)*state.iterations());
+}
+
 #define ARGS \
     ->Arg(1<<22)
 
@@ -148,5 +195,6 @@ BENCHMARK(BM_multiply) ARGS;
 BENCHMARK(BM_add_multiply) ARGS;
 BENCHMARK(BM_add_multiply_sub_shift) ARGS;
 BENCHMARK(BM_instructions) ARGS;
+BENCHMARK(BM_instructions8) ARGS;
 
 BENCHMARK_MAIN();
